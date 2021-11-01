fdadd685a6a7d95591223615f2557e95d845eec9
Dmytro Dorofeiev <dimadorofeyev@gmail.com>
2021-11-01 20:02:40 +0200

    Init commit

---
+++ data.tf
@@ -0,0 +1,94 @@
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+
+# Task role assume policy
+data "aws_iam_policy_document" "task_assume" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+# Task logging privileges
+data "aws_iam_policy_document" "task_permissions" {
+  statement {
+    effect = "Allow"
+
+    resources = [
+      aws_cloudwatch_log_group.main.arn,
+      "${aws_cloudwatch_log_group.main.arn}:*"
+    ]
+
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+  }
+}
+
+# Task ecr privileges
+data "aws_iam_policy_document" "task_execution_permissions" {
+  statement {
+    effect = "Allow"
+
+    resources = [
+      "*",
+    ]
+
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+  }
+}
+
+data "aws_kms_key" "secretsmanager_key" {
+  count = var.create_repository_credentials_iam_policy ? 1 : 0
+
+  key_id = var.repository_credentials_kms_key
+}
+
+data "aws_iam_policy_document" "read_repository_credentials" {
+  count = var.create_repository_credentials_iam_policy ? 1 : 0
+
+  statement {
+    effect = "Allow"
+
+    resources = [
+      var.repository_credentials,
+      data.aws_kms_key.secretsmanager_key[0].arn,
+    ]
+
+    actions = [
+      "secretsmanager:GetSecretValue",
+      "kms:Decrypt",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "tasks_execution_ssm" {
+  statement {
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${var.ssm_allowed_parameters}",
+      "${var.kms_key_arn}"
+    ]
+
+    actions = [
+      "ssm:GetParameters",
+      "ssm:DescribeParameters",
+      "kms:Decrypt"
+
+    ]
+  }
+}
